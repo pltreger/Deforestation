cabca2e59d5497972e5109942db6afe17ff5d0b3
Marie <marie.jassigneux@etu.univ-lyon1.fr>
2021-01-10 20:40:01 +0100

    Ajout de bouton pour afficher en fonction de l'année

--- src/grapheDeforestation/grapheDeforestation.js
+++ src/grapheDeforestation/grapheDeforestation.js
@@ -6,11 +6,12 @@
 // Pensez aussi a adapter les tailles et tout à ce que vous voulez rendre !
 
 // Definition de la taille du svgDefo (width et height)
-var marginDefo = { top: -340, right: 10, bottom: 20, left: 10 };
+var marginDefo = { top: -340, right: 10, bottom: 0, left: 10 };
 var wDefo = 400 - marginDefo.left - marginDefo.right;
 var hDefo = 400 - marginDefo.top - marginDefo.bottom;
 var radius = Math.min(wDefo, hDefo) / 2;
 var csvData;
+var dateActu = "2019";
 
 var svgDefo = d3.select("#grapheDeforestation")
 	.append("svg")
@@ -35,26 +36,45 @@ var sousTitre = svgDefo
 var pieDefo = d3.pie()
 	.padAngle(0.005)
 	.sort(null)
-	.value(d => {if(d.annee === "2019") return d.perte_surface_ha})
+	.value(d => {if(d.annee === dateActu) return d.perte_surface_ha})
 
 var outerArc = d3.arc()
   	.innerRadius(radius*1.2)
   	.outerRadius(radius)
 
-var dateArray = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"]; 
+//var dateArray = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"]; 
 
 d3.csv("https://raw.githubusercontent.com/pltreger/Deforestation/main/data/perte_couverture_mondiale_par_causes.csv").then(function(data) {
 	console.log(data)
-	var arcsDefo = pieDefo(data);
+	csvData = data;
+	update(dateActu)
+})
+
+function update(annee) {
+
+	svgDefo
+		.selectAll('path')
+		.remove()
+	svgDefo
+		.selectAll('tspan')
+		.remove()
+	svgDefo
+		.selectAll('polyline')
+		.remove()
+	console.log(annee)
+	dateActu = annee;
+	var arcsDefo = pieDefo(csvData);
 
 	var colorDefo = d3.scaleOrdinal()
-      .domain(data.map(d => {if(d.annee === "2019") return d.cause}))
+      .domain(csvData.map(d => {if(d.annee === annee) return d.cause}))
 	  .range(["#54478C","#EFEA5A","#0DB39E","#2C699A","#B9E769","#F29E4C"]);
 	  
 	svgDefo.selectAll("path")
 	  .data(arcsDefo)
 	  .enter()
 	  .append("path")
+	  .transition()
+	  .duration(1000)
 		.attr("fill", d => colorDefo(d.data.cause))
 		.attr("d", d3.arc().innerRadius(radius * 0.67).outerRadius(radius))
 
@@ -67,7 +87,7 @@ d3.csv("https://raw.githubusercontent.com/pltreger/Deforestation/main/data/perte
 		  .style("fill", "none")
 		  .attr("stroke-width", 1)
 		  .attr('points', function(d) {
-			if(d.data.annee === "2019") {
+			if(d.data.annee === annee) {
 				var posA = d3.arc().innerRadius(radius * 0.67).outerRadius(radius).centroid(d) // line insertion in the slice
 				var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
 				var posC = outerArc.centroid(d); // Label position = almost the same as posB
@@ -78,7 +98,7 @@ d3.csv("https://raw.githubusercontent.com/pltreger/Deforestation/main/data/perte
 		  })
 	  
 	  // Add the polylines between chart and labels:
-	  svgDefo
+	svgDefo
 		.selectAll('allLabels')
 		.data(arcsDefo)
 		.enter()
@@ -93,7 +113,7 @@ d3.csv("https://raw.githubusercontent.com/pltreger/Deforestation/main/data/perte
 			.call(text => text.append("tspan")
 				.attr("y", "-0.4em")
 				.attr("font-weight", "bold")
-				.text(d => {if(d.data.annee === "2019") return d.data.cause}))
+				.text(d => {if(d.data.annee === annee) return d.data.cause}))
 			.style('text-anchor', function(d) {
 					var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
 					return (midangle < Math.PI ? 'start' : 'end')
@@ -102,10 +122,9 @@ d3.csv("https://raw.githubusercontent.com/pltreger/Deforestation/main/data/perte
 				.attr("x", 0)
 				.attr("y", "0.7em")
 				.attr("fill-opacity", 0.7)
-				.text(d => {if(d.data.annee === "2019") return d.data.perte_surface_ha.toLocaleString()})
+				.text(d => {if(d.data.annee === annee) return (d.data.perte_surface_ha.toLocaleString() + " ha")})
 			.style('text-anchor', function(d) {
 				var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
 				return (midangle < Math.PI ? 'start' : 'end')
 			}))
-})
-
+}
